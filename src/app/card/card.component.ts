3ca90997e65133138fb7356aa0a04519bbd3d71a
andialif <andialif2015@gmail.com>
2022-06-03 17:15:51 +0800

    add interface

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
+import { dataCard } from './data-card';
 
 @Component({
   selector: 'app-card',
@@ -7,27 +8,24 @@ import { Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  @Input() dataPerson: {
-    index: string;
-    picture: string;
-    age: number;
-    name: string;
-    company: string;
-    phone: string;
-    address: string;
-    countLikes?: number;
-  };
 
-  @Output() parentFunction: EventEmitter<any> = new EventEmitter();
-  onLike(){
-    this.parentFunction.emit(this.dataPerson);
-  }
+
+
 
   constructor() {
 
    }
 
+
+
   ngOnInit(): void {
 
+  }
+
+  @Input() dataPerson: dataCard;
+
+  @Output() parentFunction: EventEmitter<any> = new EventEmitter();
+  onLike(){
+    this.parentFunction.emit(this.dataPerson);
   }
 }
---
+++ src/app/card/data-card.ts
@@ -0,0 +1,10 @@
+export interface dataCard{
+  index: string;
+  picture: string;
+  age: number;
+  name: string;
+  company: string;
+  phone: string;
+  address: string;
+  countLikes?: number;
+};
